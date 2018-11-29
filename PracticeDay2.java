90c7a54b8439ce3ffba8f907cdaff37d5f997b01
Riya <riyasen9876@gmail.com>
2018-11-29 10:08:53 -0500

    Update PracticeDay2.java

--- PracticeDay2.java
+++ PracticeDay2.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
 public class PracticeDay2{
 	public static void main (String[]args){
+		System.out.println(convertToArrayList(8950));
+	}
+	public static ArrayList <Integer> convertToArrayList (int x){
+		ArrayList <Integer> a = new ArrayList<>();
+		for (int i = 1; i <= 4; i ++){
+			a.add(0,x%10);
+			x/=10;
+		}
+		return a;
 	}
 }
